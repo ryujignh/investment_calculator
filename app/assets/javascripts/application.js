04d82414a784faf50d6c5cb4abc59931801ced27
Ryuji Ganaha <RyujiGanaha@MacBook-Pro-4.local>
2018-01-14 16:34:35 +0900

    installing bootstrap for rails

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,8 @@
 //
 //= require rails-ujs
 //= require turbolinks
-//= require jquery
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
 //= require jquery_ujs
 //= require_tree .
