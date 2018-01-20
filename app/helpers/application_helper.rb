73936894d011004acfb1fb1cb85cd2542feb80c4
Ryuji Ganaha <ryujignh@gmail.com>
2018-01-20 12:26:30 +0900

    fixing page title [ci skip]

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,20 +2,20 @@ module ApplicationHelper
 
   def default_meta_tags
     {
-      title: "Index investing calculator",
-      description: "A simple calculator for index investment",
-      keywords: "Index fund, Index investment, Index investing, calculator, compound, compound calculator",
+      title: "Investment calculator",
+      description: "A simple investment calculator",
+      keywords: "Investment, Index investment, calculator, compound, compound calculator",
       # icon: image_url("favicon.ico"), # favicon
       noindex: ! Rails.env.production?, # production環境以外はnoindex
       charset: "UTF-8",
       # OGPの設定
       og: {
-        title: "Index investing calculator",
+        title: "Investment calculator",
         type: "website",
         url: request.original_url,
         # image: image_url("OGPで設定する画像"),
-        site_name: "Index investing calculator",
-        description: "A simple calculator for index investment",
+        site_name: "Investment calculator",
+        description: "A simple investment calculator",
         locale: "ja_JP"
       }
     }
